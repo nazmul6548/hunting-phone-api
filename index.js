0817bbf438b2f2e3781a2fbc210b5c883ea81c08
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-28 00:51:44 +0600

    task

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const myFunc = async (searchText) => {
+const myFunc = async (searchText,isshowall) => {
   let myLink = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   );
@@ -6,20 +6,24 @@ const myFunc = async (searchText) => {
   let documents = await myLink.json();
   //    console.log(documents.data);
   const phones = documents.data;
-  displyCard(phones);
+  displyCard(phones,isshowall);
 };
 
-const displyCard = (phones) => {
+const displyCard = (phones,isshowall) => {
 
     const phoneContainer = document.getElementById('amarid');
     phoneContainer.textContent="";
+    // 
      const callany = document.getElementById("viewall")
-    if (phones.length > 12) {
+    if (phones.length > 12 && !isshowall) {
         callany.classList.remove("hidden");
     }else{
         callany.classList.add("hidden");
     }
-    phones=phones.slice(0,12)
+    
+    if (!isshowall) {
+        phones=phones.slice(0,12);
+    }
   phones.forEach((element) => {
     console.log(element);
     const phoneCards =document.createElement("div");
@@ -27,27 +31,62 @@ const displyCard = (phones) => {
     phoneCards.className = `card p-4 bg-base-100 shadow-xl`;
     phoneCards.innerHTML =`
     <figure><img src="${element.image}" alt="Shoes" /></figure>
-    <div class="card-body">
-    <h1 class=text-4xl> Brand : ${element.brand}</h1>
+    <div class="card-body flex justify-center items-center">
+    <h1 class=text-4xl text-center"> Brand : ${element.brand}</h1>
       <h2 class="card-title">${element.phone_name}</h2>
-      <p>${element.slug}</p>
-      <div class="card-actions justify-end">
-        <button class="btn btn-primary">Buy Now</button>
+      <p class="text-center">${element.slug}</p>
+      <div class="card-actions justify-center">
+        <button onclick="handleshowdetails('${element.slug}')" class="btn btn-primary">Show Details</button>
       </div>
     </div>
     `
 
     phoneContainer.appendChild(phoneCards)
   });
+  loadingSpineName(false);
 };
+
+
+const handleshowdetails = async (id) => {
+const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+const data = await res.json();
+console.log(data);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 // handle button click
-const buttonhandeler = () => {
+const buttonhandeler = (isshowall) => {
     // console.log("search");
+    loadingSpineName(true);
     const search = document.getElementById('myinput');
     const searchText = search.value;
     search.value="";
     
     // console.log(searchText);
-    myFunc(searchText);
+    myFunc(searchText,isshowall);
 };
+
 // myFunc();
+const loadingSpineName = (loading) => {
+    const loaderid = document.getElementById('lodingspine');
+    if (loading) {
+        loaderid.classList.remove("hidden");
+    }else{
+        loaderid.classList.add("hidden");
+    }
+
+}
+const showallelement = () => {
+    buttonhandeler(true)
+};
